a0d7183d854433f0b088033e3e45c85c5e6a1615
manjarrescarlos <carlosmanjarresmunoz>
2025-02-11 10:14:20 -0500

    se agrega componete comun footer y se crea file dashboard en pages

---
+++ src/components/common/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+export function Footer() {
+  return (
+    <>
+      <div class="card">
+        <div class="card-header">reserva tu habitación</div>
+        <div class="card-body">
+          <h5 class="card-title">reservas</h5>
+          <p class="card-text">
+            bienvenido a hotel aguita de peo
+          </p>
+          <a href="#" class="btn btn-primary">
+            registrate aqui
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
