2ac96ad44a484f85ccf0876d8a0b8d87b29b95b5
Juan Diego Sanchez <juandiegosanchex21@gmail.com>
2025-02-12 14:31:29 -0500

    Update: 12/02/2025

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import "./Footer.css";
 
 const Footer = () => {
     return (
-        <footer className='footer-the-wellhal-resort-and-hotel'>
+        <footer className='footer'>
+            <div className='footer__brand'>
+                <h2 className='footer__brand-name'>The Wellhall</h2>
+            </div>
+            <div className='footer__contact'>
+                <div className='footer__contact-info'>
+                    <p className='footer__contact-title'>Reservations Office</p>
+                    <div className='footer__contact-item'>
+                        <div className='footer__contact-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="footer__icon" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
+                            </svg>
+                        </div>
+                        <p className='footer__contact-text'>123 Anywhere St., Any City ST 12345</p>
+                    </div>
+                    <div className='footer__contact-item'>
+                        <div className='footer__contact-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="footer__icon" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
+                            </svg>
+                        </div>
+                        <p className='footer__contact-text'>1123-456-7890</p>
+                    </div>
+                    <div className='footer__contact-item'>
+                        <div className='footer__contact-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="footer__icon" viewBox="0 0 16 16">
+                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
+                            </svg>
+                        </div>
+                        <p className='footer__contact-text'>hello@reallygreatsite.com</p>
+                    </div>
+                </div>
+                <div className='footer__contact-info'>
+                    <p className='footer__contact-title'>Office Hours</p>
+                    <div className='footer__hours-item'>
+                        <p className='footer__hours-day'>Monday to Friday</p>
+                        <p className='footer__hours-time'>9:00 am to 6:00pm</p>
+                    </div>
+                    <div className='footer__hours-item'>
+                        <p className='footer__hours-day'>Saturday</p>
+                        <p className='footer__hours-time'>9:00 am to 12:00 noon</p>
+                    </div>
+                </div>
+                <div className='footer__contact-info'>
+                    <p className='footer__contact-title'>Get Social</p>
+                    <div className='footer__social-icons'>
+                        <div className='footer__social-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="footer__icon" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                            </svg>
+                        </div>
+                        <div className='footer__social-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="footer__icon" viewBox="0 0 16 16">
+                                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
+                            </svg>
+                        </div>
+                        <div className='footer__social-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="footer__icon" viewBox="0 0 16 16">
+                                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className='footer__social-tagline'>
+                        <p>Tag us in your photos!</p>
+                    </div>
+                </div>
+            </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
