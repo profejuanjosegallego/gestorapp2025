d616acef90951841a7561375ae3de495684ffe44
Juan Diego Sanchez <juandiegosanchex21@gmail.com>
2025-02-20 14:37:02 -0500

    Update: 20/02/2025

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css"
 import { BrowserRouter } from 'react-router-dom'
-import Router from '../src/components/router/Router.jsx'
+import Router from './components/router/Router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
