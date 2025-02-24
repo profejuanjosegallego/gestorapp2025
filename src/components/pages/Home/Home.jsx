3109b763ac5f9ef8d25919670f9a462d3370c30d
Juan Diego Sanchez <juandiegosanchex21@gmail.com>
2025-02-24 18:18:36 -0500

    Update 2: 24/02/2025

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -30,6 +30,53 @@ const Home = () => {
             </section>
             <Header />
             <AboutUs ref={aboutUsRef} />
+
+            <div className="room">
+                <div className="room__info">
+                    <h2 className="room__title">The Family <br /> Suite</h2>
+                    <p className="room__description">
+                        Showcase the best your property has to offer by highlighting one of your accommodations.
+                        Add a flattering photo, then describe the room's best feature.
+                    </p>
+                </div>
+                <div className="room__media">
+                    <div className="room__image-wrapper">
+                        <img className="room__image" src={roomPhoto} alt="Family Suite" />
+                        <div className="room__caption">
+                            <p className="room__caption-text">
+                                An intriguing caption that describes the room goes here.
+                                <br />
+                                Use a flattering photo, then describe away!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="gallery">
+                <div className="gallery__container">
+                    <div className="gallery__item">
+                        <img className="gallery__image" src={diningRoom} alt="Gallery Image 1" />
+                    </div>
+                    <div className="gallery__caption">
+                        <p className="gallery__text">
+                            An intriguing caption that describes the room goes here.
+                            Use a flattering photo, then describe away!
+                        </p>
+                    </div>
+                </div>
+                <div className="gallery__container gallery__container--highlighted">
+                    <div className="gallery__item">
+                        <img className="gallery__image gallery-image--highlighted" src={bathRoom} alt="Gallery Image 2" />
+                    </div>
+                    <div className="gallery__caption">
+                        <p className="gallery__text">
+                            An intriguing caption that describes the room goes here.
+                            Use a flattering photo, then describe away!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <InThePress />
         </>
     );
 };
