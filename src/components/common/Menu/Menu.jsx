c2778b0d9d6a2197d2f90af44800155968faa71c
mabedoya <cesde.alejandra@gmail.com>
2025-02-25 08:51:27 -0500

    Se modifica el Dashboard para incluir la opción de otros servicios

--- src/components/common/Menu/Menu.jsx
+++ src/components/common/Menu/Menu.jsx
@@ -11,14 +11,14 @@ export function Menu() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" to="/">Inicio</Link>
+              <Link className="nav-link active" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" to="/dashboard">Reservas</Link>
+              <Link className="nav-link active" to="/dashboard">Booking</Link>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                Cabañas
+                Glamping
               </a>
               <ul className="dropdown-menu">
                 <li><Link className="dropdown-item" to="/cabanas/el-encanto">El Encanto</Link></li>
--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
-import "./Dashboard.css";
+import "./Dashboard.css"; 
 
 export function Dashboard() {
   return (
-    <div className="dashboard-container">
-      <div className="dashboard-content">
-      
-        <img src="https://i0.wp.com/botanicoglamping.com/wp-content/uploads/2022/09/DSC08523-1-scaled.jpg?resize=1200%2C900&ssl=1" alt="GestorApp" className="dashboard-image" />
-
-        {/* Contenido al lado derecho */}
-        <div className="dashboard-text">
-          <h1>GestorApp</h1>
+    <div className="container dashboard-container">
+      <div className="row justify-content-center">
+        {/* Columna izquierda - Reservas de Cabañas */}
+        <div className="col-md-5 text-center">
+          <img src="https://img.alohacamp.com/0tLcDnaH-dP4zOs3yQJz5-PKNTaQZXuFuRifFR4CaTg/resize:fill:514:334/enlarge:1/czM6Ly9hbG9oYS5pbWFnZXMvcHJvcGVydGllcy9kMTc3ZmI4NWY1NzA3NGZlNDQ3ZmExMWU4Mjc0ZDA5ZThjMGViODBhLmpwZw" alt="Cabaña" className="img-fluid rounded" />
+          <h2 className="mt-3">GestorApp</h2>
           <p>Reserva la cabaña perfecta para tu descanso.</p>
           <Link to="/reservar" className="btn btn-primary">Reservar</Link>
+        </div>
+
+        {/* Columna derecha - Otros Servicios */}
+        <div className="col-md-5 text-center">
+          <h2 className="mt-3">Otros Servicios</h2>
+          <p><strong>Spa:</strong> Relájate con nuestros servicios de spa exclusivos.</p>
+          <p><strong>Horarios de Atención:</strong></p>
+          <ul className="list-unstyled">
+            <li>Lunes a Viernes: 9:00 AM - 8:00 PM</li>
+            <li>Sábados y Domingos: 10:00 AM - 6:00 PM</li>
+          </ul>
         </div>
       </div>
     </div>
   );
 }
+
+
