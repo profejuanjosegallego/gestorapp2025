c885023674dbee4df47d297edcb5fd7562d387d6
Diego Andres Bravo <Diegobravo.c96@gmail.com>
2025-04-28 11:53:03 -0500

    consumo api pexels

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ReservasProvider } from '../src/Contexts/reservascontext'
+import { ReservasProvider } from './Contexts/reservascontext.jsx'
 import { Router } from './components/Router/Router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
